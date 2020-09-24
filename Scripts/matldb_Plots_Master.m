d2872468e8eda9691022c5c25a917c5907f03b73
rmcdermo <randy.mcdermott@gmail.com>
2020-09-24 05:17:20 -0400

    Scripts: add master plotting script, edit READMEs

---
+++ Scripts/matldb_Plots_Master.m
@@ -0,0 +1,15 @@
+% McDermott
+% 9-24-2020
+% matldb_Plots_Master.m
+
+close all
+clear all
+
+disp('Import_data...');           Import_data
+disp('Cone_Analysis...');         Cone_Analysis
+disp('DSC_Analysis...');          DSC_Analysis
+disp('Gasification_Analysis...'); Gasification_Analysis
+disp('MCC_Analysis...');          MCC_Analysis
+disp('TGA_Analysis...');          TGA_Analysis
+
+disp('All done!')
