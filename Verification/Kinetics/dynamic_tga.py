734192ed3fd1cc1ae4a468432187d36b4cce0356
juancgauna <126920123+juancgauna@users.noreply.github.com>
2023-07-13 16:15:19 -0400

    json importing , formatting
    Corrected json file path in dynamic_tga.  iso_tga now imports kinetic parameters from MaCFP_PMMA_NIST.  new iso_tga_data.csv for new results using imported kinetic parameters.  formatting.

--- Verification/Kinetics/dynamic_tga.py
+++ Verification/Kinetics/dynamic_tga.py
@@ -3,123 +3,119 @@ Script to generate exact solution for constant heating rate TGA
 using the solution from Coheur et al., J Mater Sci, 2021
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.special import expi
-import csv
-import pandas as pd
 import json
+import csv
+import numpy             as np
+import matplotlib.pyplot as plt
+import pandas            as pd
+from   scipy.special import expi
 
 
-json_file_path = '../../PMMA/Material_Properties/MaCFP_PMMA_NIST.json'
+json_file_path = '../../PMMA/Material_Properties/2021/MaCFP_PMMA_NIST.json'
+
 
 # ***replace with command line file specification
-csv_file_path = 'NIST_TGA_10K_cat_devc.csv'
+csv_file_path  = 'NIST_TGA_10K_cat_devc.csv'
 
-
+# read the json file
 with open(json_file_path, 'r') as file:
-    data = json.load(file)
-    A = data['Kinetics']['Pre-exponential']
-    E = data['Kinetics']['Activation Energy']
+    kdata           = json.load(file)
+
+# read the CSV file
+data                = pd.read_csv(csv_file_path)
 
 
-# Read the CSV file
-data = pd.read_csv(csv_file_path)
-print(data.columns)
-# Extract Imported data and replace non-number values with NaN values
-data['Time'] = pd.to_numeric(data['s'], errors='coerce')
-data['Mass'] = pd.to_numeric(data['[mg]'], errors='coerce')
-data['Temperature'] = pd.to_numeric(data['[K]'], errors='coerce')
-data['Heat Flow'] = pd.to_numeric(data['[W/g]'], errors='coerce')
+# extract imported data and replace non-number values with NaN values
+data['Time']        = pd.to_numeric(data['s']    , errors = 'coerce')
+data['Mass']        = pd.to_numeric(data['[mg]'] , errors = 'coerce')
+data['Temperature'] = pd.to_numeric(data['[K]']  , errors = 'coerce')
+data['Heat Flow']   = pd.to_numeric(data['[W/g]'], errors = 'coerce')
 
 # Drop NaN values
-data = data.dropna()
+data    = data.dropna()
+
 
 # model predictions 
-t_m = data['Time'].values
-m_m = data['Mass'].values
-T_m = data['Temperature'].values
-beta_m = data['Heat Flow'].values
+t_m     = data['Time'].values
+m_m     = data['Mass'].values
+T_m     = data['Temperature'].values
+beta_m  = data['Heat Flow'].values
+
+
+#kinetic parameters
+A       = kdata['Kinetics']['Pre-exponential']   # pre-exponential    , 1/s
+E       = kdata['Kinetics']['Activation Energy'] # activation energy  , J/mol
 
 # constant
-R       = 8.314             # gas constant, J/mol-K
+R       = 8.314                                  # gas constant       , J/mol-K
 
-# kinetic parameters
-#A       = 2.85e13              # pre-exponential, 1/s
-#E       = 1.91e5           # activation energy, J/mol
-m_f     = m_m[-1]                 # final mass
+# kinetic parameter
+m_f     = m_m[-1]                                # final mass
 
 # scenario parameters
-#m_0     = 1                 # initial mass
-T_0     = T_m[0]                # initial temperature, C
-beta    = 10                # heating rate, K/min
-t_f     = t_m[-1]              # final time, s
-
-
+T_0     = T_m[0]                                 # initial temperature, C
+beta    = 11                                     # heating rate       , K/min
+t_f     = t_m[-1]                                # final time         , s
+alpha_0 = 0                                      # initial progress factor
 
 # unit conversions
-beta    = beta/60;          # heating rate, K/s
-#T_0     = T_0 + 273.15;     # initial temperature, K
-alpha_0 = 0                 # initial progress factor
-
+beta    = beta / 60    ;                         # heating rate       , K/s
+#T_0    = T_0  + 273.15;                         # initial temperature, K
 
 # numerical parameters
-N = len(t_m)
+N       = len(t_m)
 
 
 # create solution arrays
 t       = np.linspace(0, t_f, N)
-#T       = T_0 + beta[i]*t
-alpha   = np.zeros( N )
+alpha   = np.zeros(N)
 
 
 # compute C constant
-C = (1 - alpha_0)*np.exp( (A/beta)*T_0*np.exp(-E/(R*T_0)) + \
-            A*E/(beta*R)*expi(-E/(R*T_0)))
+C       = (1 - alpha_0) * np.exp( (A / beta) * T_0 * np.exp(-E / (R * T_0) )   + \
+                A * E / (beta * R) * expi( - E / (R * T_0) ) )
 
 
 # calculate progress factors at all times
 for i in range(0, N):
 
-    alpha[i] = 1 - C*np.exp(-(A/beta)*T_m[i]*np.exp(-E/(R*T_m[i])) - \
-                (A/beta)*(E/R)*expi(-E/(R*T_m[i])))
+    alpha[i] = 1 - C * np.exp( - (A / beta) * T_m[i] * np.exp( - E / (R * T_m[i]) ) - \
+               (A / beta) * (E / R) * expi( - E / (R * T_m[i]) ) )
+
 
 # convert alpha to masses
-m_e       = m_m[0] - (m_m[0] - m_f)*alpha
+m_e          = m_m[0] - (m_m[0] - m_f) * alpha
+
 
 # plotting parameters
-#plt.rc('text', usetex=True)
-#plt.rc('font', family='serif')
-#plt.rc('lines', linewidth=1.5)
-#plt.rc('xtick', labelsize=18)
-#plt.rc('ytick', labelsize=18)
+plt.rc('text' , usetex    = True)
+plt.rc('font' , family    = 'serif')
+plt.rc('xtick', labelsize = 18)
+plt.rc('ytick', labelsize = 18)
 
 
+# plot imported data
+plt.plot(T_m, m_m, label  = 'Model Predictions', color = 'red', marker = '.')
 
+# plot the results
+plt.plot(T_m, m_e, label  = 'Exact Solution')
 
-# Plot the Imported data
-plt.plot(T_m, m_m, label='Model Predictions', color='red', marker='.')
-
-# Plot the results
-plt.plot(T_m, m_e, label='Exact Solution')
-
-plt.xlabel(r'Temperature (K)', fontsize=20)
-plt.ylabel(r'Mass (-)', fontsize=20)
+plt.xlabel(r'Temperature (K)', fontsize = 20)
+plt.ylabel(r'Mass (-)'       , fontsize = 20)
 plt.legend()
 plt.tight_layout()
 plt.show()
 
 
-##Save as CSV
-#with open('Equation_33_data.csv', mode='w', newline='') as file:
+ # save as CSV
+#with open('dynamic_tga.csv', mode='w', newline='') as file:
 #    writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#    writer.writerow(['Time', 'a', 'Temperature'])
-#    writer.writerow(['s', 'a', 'K'])
-#    for i in range(len(T)):
-#        writer.writerow([tlist[i], alist[i], T[i]])
+#    writer.writerow(['Time', 'alpha', 'Temperature'])
+#    writer.writerow([   's',     '-',           'K'])
+#    for i in range(len(t_m)):
+#        writer.writerow([ t[i], alpha[i], T_m[i] ])
 
-# Sum of square errors are calculated directly
-#sse_mass = np.sum((m_m - m_e) ** 2)
-rms_err = np.sqrt(np.sum((m_m - m_e) ** 2)/N)
-#print('Sum of squared errors for mass:', sse_mass)
-print('Root Mean Square Error:', rms_err)
+
+# root mean square error is calculated
+rms_err    = np.sqrt(np.sum( (m_m - m_e) ** 2) / N)
+print('root mean square error:', rms_err)
--- Verification/Kinetics/iso_tga.py
+++ Verification/Kinetics/iso_tga.py
@@ -2,86 +2,96 @@
 Script to generate exact solution for isothermal TGA
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
 import csv
-import pandas as pd
+import json
+import numpy             as np
+import matplotlib.pyplot as plt
+import pandas            as pd
+
+
+json_file_path = '../../PMMA/Material_Properties/2021/MaCFP_PMMA_NIST.json'
+
 
 # ***replace with command line file specification
-csv_file_path = 'iso_tga_data.csv'
+csv_file_path  = 'iso_tga_data.csv'
 
-# constant
-R = 8.134  # gas constant, J/mol-K
 
-# Kinetic parameters
-A = 4e12    # pre-exponential, 1/s
-E = 2e5     # activation energy, J/mol
+# read the json file
+with open(json_file_path, 'r') as file:
+    kdata            = json.load(file)
 
-# scenario parameters
-m_0 = 1  # initial mass
-T = 700  # constant temperature, K
-#t_f = 1800  # final time, s
 
-# Read the CSV file
-data = pd.read_csv(csv_file_path)
+# read the CSV file
+data                = pd.read_csv(csv_file_path)
 
-# Extract Imported data and replace non-number values with NaN values
-data['Time'] = pd.to_numeric(data['Time'], errors='coerce')
-data['Mass'] = pd.to_numeric(data['Mass'], errors='coerce')
-data['Temperature'] = pd.to_numeric(data['Temperature'], errors='coerce')
 
-# Drop NaN values
-data = data.dropna()
+# extract imported data from CSV and replace non-number values with NaN values
+data['Time']        = pd.to_numeric(data['Time']       , errors = 'coerce')
+data['Mass']        = pd.to_numeric(data['Mass']       , errors = 'coerce')
+data['Temperature'] = pd.to_numeric(data['Temperature'], errors = 'coerce')
+
+# drop NaN values
+data                = data.dropna()
+
 
 # model predictions
-t_m = data['Time'].values
-m_m = data['Mass'].values
-T_m = data['Temperature'].values
+t_m  = data['Time'].values
+m_m  = data['Mass'] .values
+T_m  = data['Temperature'].values
+
+
+# kinetic parameters
+A    = kdata['Kinetics']['Pre-exponential']   # pre-exponential     , 1/s
+E    = kdata['Kinetics']['Activation Energy'] # activation energy   , J/mol
+
+# constants
+R    = 8.134                                  # gas constant        , J/mol-K
+
+# scenario parameters
+m_0  = 1                                      # initial mass
+T    = 700                                    # constant temperature, K
+#t_f = 1800                                   # final time          , s
 
 # numerical parameters
-N = len(t_m)
-#N = 500  # number of data points
+N    = len(t_m)
 
 # create solution arrays
-#t = np.linspace(0, t_f, N)
-m_e = np.zeros(N)
-
+m_e  = np.zeros(N)
 
 
 # calculate the mass at all times
 for i in range(0, N):
-    m_e[i] = m_0 * np.exp(-A * np.exp(-E / (R * T)) * t_m[i])
+    m_e[i] = m_0 * np.exp( - A * np.exp( - E / (R * T)) * t_m[i])
+
 
 # plotting parameters
-# If receiving tex/latex error try removing plotting parameters
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-plt.rc('lines', linewidth=1.5)
-plt.rc('xtick', labelsize=18)
-plt.rc('ytick', labelsize=18)
+plt.rc('text' , usetex    = True)
+plt.rc('font' , family    = 'serif')
+plt.rc('lines', linewidth = 1.5)
+plt.rc('xtick', labelsize = 18)
+plt.rc('ytick', labelsize = 18)
 
 # Plot the Imported data
-plt.plot(t_m, m_m, label='Model Predictions', color='red', marker='.')
+plt.plot(t_m, m_m, label  = 'Model Predictions', color = 'red', marker = '.')
 
-# Plot the results
-plt.plot(t_m, m_e, label='Exact Solution')
-
-plt.xlabel(r'Time (s)', fontsize=20)
-plt.ylabel(r'Mass (-)', fontsize=20)
+# plot the results
+plt.plot(t_m, m_e, label  = 'Exact Solution')
+plt.xlabel(r'Time (s)', fontsize = 20)
+plt.ylabel(r'Mass (-)', fontsize = 20)
 plt.legend()
 plt.tight_layout()
 plt.show()
 
-# Save as CSV
-# with open('iso_tga_data.csv', mode='w', newline='') as file:
-#    writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#    writer.writerow(['Time', 'Mass', 'Temperature'])
-#    writer.writerow(['[s]', '[-]', 'K'])
-#    for i in range(N):
-#        writer.writerow([t[i], m[i], T[i]])
 
-# Sum of square errors are calculated directly
-#sse_mass = np.sum((m_m - m_e) ** 2)
-rms_err = np.sqrt(np.sum((m_m - m_e) ** 2)/N)
-#print('Sum of squared errors for mass:', sse_mass)
-print('Root Mean Square Error:', rms_err)
+ # save as CSV
+#with open('iso_tga_data.csv', mode = 'w', newline = '') as file:
+#    writer = csv.writer(file, delimiter = ', ', quotechar='"', quoting = csv.QUOTE_MINIMAL)
+#    writer.writerow(['Time' , 'Mass', 'Temperature'])
+#    writer.writerow(['[s]'  , '[-]' , 'K'])
+#    for i in range(N):
+#        writer.writerow([t_m[i], m_e[i], T])
+
+
+#calculates root mean square error
+rms_err = np.sqrt(np.sum( (m_m - m_e) ** 2) / N)
+print('root mean square error:', rms_err)
