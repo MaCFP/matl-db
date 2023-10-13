3a25f4876022d1b320e9be0de5adf3afdb95d37f
Morgan Bruns <morgan.chase.bruns@gmail.com>
2023-10-13 13:31:55 -0500

    cleaned up Master.py

--- Verification/Scripts/Master.py
+++ Verification/Scripts/Master.py
@@ -1,26 +1,23 @@
+"""
+
+Master script to run verification cases
+
+"""
+
 import subprocess
 import argparse
-import json
-import os
 
 # create the parser
 parser = argparse.ArgumentParser()
+
 # add an argument
-parser.add_argument('experiment')
-parser.add_argument('material')
+parser.add_argument('scenario')     # scenario to be considered
+parser.add_argument('matl_set')     # material property set to be validated
+
 # parse arguments
 args = parser.parse_args()
 
-subprocess.run(["python", args.experiment + ".py", args.material])
+# running exact solution of scenario
+subprocess.run(["python", args.scenario + ".py", args.matl_set])
 
-
-# remove results.csv file
-#try:
-#    os.remove('../results/' + args.material + "_" + "dynamic_TGA_10K" + "_" + "FDS" + "_results.csv")
-#except(FileNotFoundError):
-#    pass
-            
-            
-            
-            
-            
+           
