3087b19945289534f16cda02e4ec9b85dfbf5422
juancgauna <126920123+juancgauna@users.noreply.github.com>
2024-01-09 17:08:50 -0500

    Modified dynamic_tga python script to save analytical solution vs model prediction plot as pdf in plot results folder.  Rewrote Master.py automate the generation.  Used analytical solution with reaction order from Coheurt paper to create peicewise functions in python and create function within python using MP package. which solves the non-linear equations for the reaction advancedment, and therefore mass.  Cleaned up dynamic_tga and Master python scripts.

--- Verification/Scripts/Master.py
+++ Verification/Scripts/Master.py
@@ -10,6 +10,7 @@ import argparse
 # create the parser
 parser = argparse.ArgumentParser()
 parser.add_argument('--compare_all', "-c", action = "store_true")
+parser.add_argument('--generate_all_fds', "-g", action = "store_true")
 # add an argument
 #parser.add_argument('scenario')     # scenario to be considered
 #parser.add_argument('matl_set')     # material property set to be validated
@@ -17,7 +18,8 @@ parser.add_argument('--compare_all', "-c", action = "store_true")
 # parse arguments
 args = parser.parse_args()
 
-if args.compare_all is True:
+if args.compare_all is True or args.generate_all_fds is True:
+    error_list = []
     matl_set_list = [
         "MaCFP_PMMA_NIST",
         "MaCFP_PMMA_Aalto_I",
@@ -44,11 +46,19 @@ if args.compare_all is True:
         "MaCFP_PMMA_NIST - StMU",
         "MaCFP_PMMA_UMET",
         ]
-    for matl_set in matl_set_list:
-        try:
-            subprocess.run(["python", "dynamic_tga" + ".py", str(matl_set), str(2021),"-co"])
-        except:
-            pass
+    if args.compare_all is True:
+        for matl_set in matl_set_list:
+            try:
+                subprocess.run(["python", "dynamic_tga" + ".py", str(matl_set), str(2021),"-co"])
+                subprocess.run(["python", "dynamic_tga" + ".py", str(matl_set), str(2023),"-co"])
+            except:
+                pass
+    if args.generate_all_fds is True:
+        for matl_set in matl_set_list:
+            try:
+                subprocess.run(["python","testing"+".py",str(matl_set)])
+            except:
+                pass
       #  try:
 # running exact solution of scenario
        #     subprocess.run(["python", dynamic_tga + ".py", matl_set, 2021])
